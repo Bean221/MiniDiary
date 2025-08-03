2170aef1de4da3b5b7ea858dc1790fe2fb74727c
thien <185050191+longthien-09@users.noreply.github.com>
2025-08-04 00:23:23 +0700

    thien

--- MiniDiary/src/main/java/com/example/MiniDiary/MiniDiaryApplication.java
+++ MiniDiary/src/main/java/com/example/MiniDiary/MiniDiaryApplication.java
@@ -2,9 +2,16 @@ package com.example.MiniDiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class MiniDiaryApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniDiaryApplication.class, args);
